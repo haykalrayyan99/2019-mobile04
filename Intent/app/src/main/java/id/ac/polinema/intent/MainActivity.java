cde0318eb63855ebd333748a594e50dbc791090d
rayyan14 <choutzuyuu14@gmail.com>
2019-09-18 15:38:40 +0700

    Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -2,7 +2,9 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void handleExplicitInternet(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View View) {
+        finish();
+    }
+
+    public void handleImplicitInternet(View View) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
     }
 
 }
